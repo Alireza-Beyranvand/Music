d1aaab9b9d92ff1d8d75c0d90a2d29c5855ef1f7
Alireza Beyranvand <167140307+Alireza-Beyranvand@users.noreply.github.com>
2024-06-12 16:53:13 +0330

    Add More

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -7,17 +7,21 @@ import { useImmer } from "use-immer";
 import { useEffect } from "react";
 import { selectStatusLoading, fetchAllbum, fetchMusics } from "../../reducers/MusicSlice";
 import { useSelector, useDispatch } from "react-redux";
+
+
+// Components
+
 import PlayerBox from "../playerBox/PlayerBox";
 import TitleMusics from "../titleMusic/TitleMusic";
 import PlayList from "../playList/PlayList";
 import ImagePlaying from "../titleMusic/imagePlaying/ImagePlaying";
 import { ToastContainer } from "react-toastify";
-
-
+import Loading from "../SpinnerLoading/Loading";
 
 const MainLayout = () => {
 
 
+    // State for Open drawer
     const [openList, setOpenList] = useImmer(false)
 
 
@@ -37,8 +41,11 @@ const MainLayout = () => {
 
     useEffect(() => {
         if (status === "none") {
-            Dispatch(fetchMusics());
-            Dispatch(fetchAllbum())
+            setTimeout(() => {
+                Dispatch(fetchMusics());
+                Dispatch(fetchAllbum())
+            }
+            , 1000)
         }
     }, [])
 
@@ -55,35 +62,46 @@ const MainLayout = () => {
 
     return (
         <>
-            <ToastContainer position="top-right"
-                autoClose={3000} hideProgressBar={false}
-                newestOnTopfalse closeOnClick draggable />
-            <div className="container AllContent" >
-                <div className="row" >
-                    <div className="col">
-                        <div className="container " >
-                            <div className="row row1" >
-                                <div className="col box text-white" >
-                                    {/* button Open Drawer */}
-                                    <button className=" btn btn-secondary buttonListMusics"
-                                        style={styleButtons("none")} onClick={OpenDrawer} > <i className="fa fa-bars" ></i></button>
-                                    {/* button close Drawer */}
-                                    <button className=" btn btn-danger buttonCloseListMusics"
-                                        style={styleButtons("inline")} onClick={OpenDrawer} > <i className="fa fa-close" ></i></button>
-                                    <ImagePlaying />
+            {status === "none" || status === "Loading" ?
+                (<Loading />)
+                :
+                (
+                    <>
+                        <ToastContainer position="top-right"
+                            autoClose={3000} hideProgressBar={false}
+                            newestOnTopfalse closeOnClick draggable />
+                        <div className="container AllContent Scroll" >
+                            <div className="row" >
+                                <div className="col">
+                                    <div className="container " >
+                                        <div className="row row1" >
+                                            <div className="col box text-white Scroll" >
+                                                {/* button Open Drawer */}
+                                                <button className=" btn btn-secondary buttonListMusics"
+                                                    style={styleButtons("none")} onClick={OpenDrawer} > <i className="fa fa-bars" ></i></button>
+                                                {/* button close Drawer */}
+                                                <button className=" btn btn-danger buttonCloseListMusics"
+                                                    style={styleButtons("inline")} onClick={OpenDrawer} > <i className="fa fa-close" ></i></button>
+                                                <ImagePlaying />
+                                            </div>
+                                            <TitleMusics />
+                                        </div>
+                                        <div className="row row2 text-white" >
+                                            <div className="col text-center borders" >
+                                                <PlayerBox />
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                                <TitleMusics />
-                            </div>
-                            <div className="row row2 text-white" >
-                                <div className="col text-center borders" >
-                                    <PlayerBox />
-                                </div>
+                                <PlayList openList={openList} OpenDrawer={OpenDrawer} />
                             </div>
                         </div>
-                    </div>
-                    <PlayList openList={openList} OpenDrawer={OpenDrawer} />
-                </div>
-            </div>
+
+                    </>
+                )
+
+
+            }
 
         </>
     )
--- src/components/firstPage/FirstPage.jsx
+++ src/components/firstPage/FirstPage.jsx
@@ -8,14 +8,14 @@ const FirstPage = () => {
 
     return (
         <>
-            <div className="container containerFirstPage ">
+            <div className="container containerFirstPage">
                 <div className="card mb-3 b backgroundFirstpage Scroll">
-                    <img className="card-img-top rounded-3 mt-4 Scroll" src={img} alt="@n.moein" />
+                    <img className="card-img-top rounded-3 mt-4 " src={img} alt="@n.moein" />
                     <div className="card-body">
-                        <h5 className="card-title text-center text-white textOne mt-2 Scroll">Moein</h5>
-                        <p className="card-text text-center text-white textTwo Scroll">The Best Musics</p>
+                        <h5 className="card-title text-center text-white textOne mt-2 ">Moein</h5>
+                        <p className="card-text text-center text-white textTwo">The Best Musics</p>
                         <p className="card-text text-center">
-                            <Link to="/MainLayout" className="btn btn-secondary shadow mt-3 mb-3 btnGo Scroll" >Go To Player &nbsp;<i className="fa fa-play iconPlay" ></i></Link>
+                            <Link to="/MainLayout" className="btn btn-secondary shadow mt-3 mb-3 btnGo" >Go To Player &nbsp;<i className="fa fa-play iconPlay" ></i></Link>
                         </p>
                     </div>
                 </div>
